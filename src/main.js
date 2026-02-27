2a46edf39df61e7cfd9ff81274d5ea49c0b0a235
djibe <djibe89@hotmail.com>
2026-02-27 16:21:56 +0100

    Update Fluent 2

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
+// import '@fluentui/web-components/'
 import { setTheme } from '@fluentui/web-components'
+
 import { webLightTheme } from '@fluentui/tokens'
 import './style.css'
 import { renderApp } from './app.js'
