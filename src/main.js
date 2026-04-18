10ac7597f1c0a3519396991f0d4259a07e05e0fa
djibe <djibe89@hotmail.com>
2026-04-18 13:46:56 +0200

    Update npm packages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 // import '@fluentui/web-components/'
-import 'https://unpkg.com/@fluentui/web-components@3.0.0-rc.10/dist/web-components.min.js'
+import 'https://unpkg.com/@fluentui/web-components@3.0.0-rc.13/dist/web-components.min.js'
 import { setTheme } from '@fluentui/web-components'
 import { webLightTheme } from '@fluentui/tokens'
 import './style.css'
